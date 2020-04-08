a458aeb58d10fb4b0a2fc033a779e9ca854feb20
Dmitriy <dmitriy.kulakov.000@gmail.com>
2020-04-08 12:09:54 +0500

    * Clean rep

--- docs/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['helper_11',['Helper',['../class_helper.html',1,'']]]
-];
--- docs/search/classes_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['mainwindow_12',['MainWindow',['../class_main_window.html',1,'']]]
-];
